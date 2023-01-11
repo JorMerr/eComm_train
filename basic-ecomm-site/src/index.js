7578e3d3c1016117ace5b72d7310e1daa470e241
JorMerr <cookandcrafter@gmail.com>
2023-01-11 17:08:31 -0500

    following tutorial, bootstrap navbar toggle not working as expected

---
+++ basic-ecomm-site/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import NavBar from "./App";
+import "./index.css";
+
+ReactDOM.render(<NavBar />, document.getElementById("root"));
